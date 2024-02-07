88843d193d5b089dcd909c0b07c259cfc23acbab
drios8331 <73044961+drios8331@users.noreply.github.com>
2024-02-06 23:45:49 -0500

    creacion de index

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./assets/images/logo.png" />
+    <title>Index</title>
+
+    <!-- JQuery -->
+    <script src="./node_modules/jquery/dist/jquery.min.js"></script>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./node_modules/bootstrap-icons/font/bootstrap-icons.min.css">
+
+    <!-- DataTables -->
+    <script type="text/javascript" src="./node_modules/datatables.net/js/jquery.dataTables.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="./node_modules/datatables.net-dt/css/jquery.dataTables.min.css">
+
+    <!-- Animate CSS -->
+    <link rel="stylesheet" href="./node_modules/animate.css/animate.min.css" />
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+
+<body>
+    <div class="content">
+        <div class="row">
+            <div class="col-xl-6 bg-primary"></div>
+            <div class="col-xl-6 bg-secondary">
+                <div class="card">
+                    <div class="card-body">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
