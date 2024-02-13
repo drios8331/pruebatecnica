8d8a33b737b9dfde4e7973875f1f399b7e4d0090
drios8331 <73044961+drios8331@users.noreply.github.com>
2024-02-13 18:02:18 -0500

    Cambios en index

--- index.php
+++ index.php
@@ -25,18 +25,44 @@
     <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 
-<body>
-    <div class="content">
-        <div class="row">
-            <div class="col-xl-6 bg-primary"></div>
-            <div class="col-xl-6 bg-secondary">
-                <div class="card">
+<body class="scrolly m-0">
+    <div id="respuesta"></div>
+    <div class="content" id="index">
+        <div class="row h-100">
+            <div class="col-xl-6 bg-primary m-0 p-0"></div>
+            <div class="col-xl-6 bg-secondary m-0 p-0">
+                <div class="card w-50 mx-5 mt-5">
+                    <div class="card-header card-title text-primary text-center">
+                        <h3>Inicio de Session</h3>
+                    </div>
                     <div class="card-body">
+                        <div class="form-floating mb-3">
+                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                            <label for="floatingInput">Email address</label>
+                        </div>
+                        <div class="form-floating">
+                            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                            <label for="floatingPassword">Password</label>
+                        </div>
+                    </div>
+                    <div class="card-footer p-0 m-0">
+                        <div class="d-greed">
+                            <button class="btn btn-primary w-100 rounded-top-0">Iniciar Sesion</button>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <script type="text/javascript">
+        $(document).ready(function() {
+
+            var height = $(window).height();
+
+            $('#index').height(height);
+        });
+    </script>
+
 </body>
 
 </html>
